3eb6d1389b27be3a7f63493fd1032f269a6ae319
Mohamed Amine FRAD <frad.amine@gmail.com>
2025-04-21 21:13:50 +0100

    Update Forms (forms.py)

--- django-todo/todo/forms.py
+++ django-todo/todo/forms.py
@@ -1,7 +1,36 @@
 from django import forms
-from .models import Task
+from .models import Task, Category, Tag
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class TaskForm(forms.ModelForm):
+    due_date = forms.DateField(
+        widget=forms.DateInput(attrs={'type': 'date'}),
+        required=False
+    )
+    
     class Meta:
         model = Task
-        fields = ['title', 'description', 'complete']
+        fields = ['title', 'description', 'complete', 'due_date', 'priority', 'category', 'tags']
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 3}),
+        }
+
+class CategoryForm(forms.ModelForm):
+    color = forms.CharField(widget=forms.TextInput(attrs={'type': 'color'}))
+    
+    class Meta:
+        model = Category
+        fields = ['name', 'color']
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = ['name']
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
